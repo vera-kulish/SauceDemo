3dc65aee0cc47854e94b832482eac05783e8be8a
vera-kulish <verakoulich@gmail.com>
2023-11-08 22:22:45 +0100

    TMS-2 locators

--- src/test/java/ProductsTest.java
+++ src/test/java/ProductsTest.java
@@ -12,7 +12,7 @@ public class ProductsTest extends BaseTest {
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
         driver.findElement(By.xpath("//*[text()='Sauce Labs Bike Light']/ancestor::*[@class='inventory_item']//button")).click();
-        
+
         driver.findElement(By.id("shopping_cart_container")).click();
         assertEquals(driver.findElement(By.cssSelector(".inventory_item_name")).getText(), "Sauce Labs Bike Light");
         assertEquals(driver.findElement(By.cssSelector(".inventory_item_price")).getText(), "$9.99");
